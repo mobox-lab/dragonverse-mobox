855fa691d0525e28f34c0c476ad35fc5c2e061ad
Tomori6657 <japancjc123@gmail.com>
2024-04-01 17:43:46 +0800

    feat: add buff dialog

--- src/components/ui/dialog/DialogComponents.tsx
+++ src/components/ui/dialog/DialogComponents.tsx
@@ -4,13 +4,17 @@ import { ClientOnly } from '@/components/common/ClientOnly';
 import DragonBurnDialog from '@/components/ui/dialog/DragonBurnDialog';
 import MainWalletConnectDialog from '@/components/ui/dialog/wallet-connect/MainWalletConnectDialog';
 import LBPRewardDialog from './LBPRewardDialog';
+import StakeBuffDialog from '@/components/ui/dialog/stake/StakeBuffDialog';
+import BtcWalletConnectDialog from '@/components/ui/dialog/wallet-connect/BtcWalletConnectDialog';
 
 export default function DialogComponents() {
   return (
     <ClientOnly>
+      <StakeBuffDialog />
+      <LBPRewardDialog />
       <DragonBurnDialog />
       <MainWalletConnectDialog />
-      <LBPRewardDialog />
+      <BtcWalletConnectDialog />
     </ClientOnly>
   );
 }
