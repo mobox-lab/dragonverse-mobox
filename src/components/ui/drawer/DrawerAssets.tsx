340b4cdeb6a956aa4caed4044b9c8dad4871bc55
joji-mb <545633740@qq.com>
2024-10-24 13:21:10 +0800

    refactor: update tailwind prettier

--- src/components/ui/drawer/DrawerAssets.tsx
+++ src/components/ui/drawer/DrawerAssets.tsx
@@ -66,14 +66,17 @@ export default function DrawerAssets() {
     setGameAssetsLogDrawer(true);
   }, []);
 
-  const onClaimGameAsset = useCallback(async (id: GameAssetID) => {
-    if (isClaimPending) {
-      return;
-    }
+  const onClaimGameAsset = useCallback(
+    async (id: GameAssetID) => {
+      if (isClaimPending) {
+        return;
+      }
 
-    await claimGameAsset({id});
-    refetchGameAsset();
-  }, [isClaimPending]);
+      await claimGameAsset({ id });
+      refetchGameAsset();
+    },
+    [isClaimPending],
+  );
 
   const openDWDrawer = (type: 'Deposit' | 'Withdraw') => {
     setDWOpen(true);
@@ -95,14 +98,14 @@ export default function DrawerAssets() {
     <Drawer
       open={isOpen}
       onOpenChange={setIsOpen}
-      className='xl:p-[30px]'
+      className="xl:p-[30px]"
       title={
         <div className="flex items-center gap-3">
-          <div className="size-15 rounded-full bg-gray overflow-hidden">
-            <img className='w-full h-full' src={userGameInfo?.gparkUserAvatar} />
+          <div className="size-15 overflow-hidden rounded-full bg-gray">
+            <img className="h-full w-full" src={userGameInfo?.gparkUserAvatar} />
           </div>
           <div>
-            <p className="text-[1.6vw]/[1.92vw] font-semibold xl:text-xl/6 truncate max-w-[10vw]">
+            <p className="max-w-[10vw] truncate text-[1.6vw]/[1.92vw] font-semibold xl:text-xl/6">
               {userGameInfo?.gparkUserName || shortenAddress(address)}
             </p>
             {userGameInfo?.gparkUserName ? (
@@ -194,7 +197,7 @@ export default function DrawerAssets() {
             </Button>
           </div>
           {/* <div className="mt-[3.84vw] text-[1.28vw]/[1.92vw] font-semibold xl:mt-12 xl:text-base/6">My Inventory</div> */}
-          <div className="flex items-center justify-between mt-[3.84vw] xl:mt-12">
+          <div className="mt-[3.84vw] flex items-center justify-between xl:mt-12">
             <div className="text-[1.28vw]/[1.92vw] font-semibold xl:text-base/6">My Inventory</div>
             <div
               className="flex cursor-pointer select-none items-center gap-1 text-[1.12vw]/[1.12vw] font-semibold text-blue xl:text-sm/3.5"
@@ -204,35 +207,54 @@ export default function DrawerAssets() {
               <HistorySVG className="w-[1.28vw] xl:w-4" />
             </div>
           </div>
-          <div className="mt-[0.96vw] h-[1px] w-full bg-white/25 xl:mt-3 mb-[2.4vw] xl:mb-7.5"></div>
-         {
-          assets.map(item => ( <div className="mb-[1.92vw] xl:mb-6" key={item.id}>
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <img src={item.icon} alt="capture-ball" className="h-[3.2vw] xl:size-10" />
-                <div className='mx-2'>
-                  <p className="text-[1.28vw]/[1.6vw] font-medium xl:text-base/5">{item.name}</p>
-                  <p className="text-[0.96vw]/[1.6vw] text-gray-300 xl:text-xs/5 w-[13vw] xl:w-[160px]">{item.describe}</p>
-                </div>
-                {gameAsset?.assets?.[item.id]?.unclaim ? <Button
-                  type="yellow-shallow-2"
-                  className="h-[2.56vw] w-[8vw] text-[1.12vw]/[1.12vw] font-medium text-yellow xl:h-8 xl:w-25 xl:text-sm/3.5 relative overflow-visible"
-                  onClick={() => onClaimGameAsset(item.id)}
-                >
-                  Claim
-                  <div className="absolute right-0 top-0 flex h-4 min-w-4 translate-x-[50%] translate-y-[-50%] items-center justify-center rounded-full bg-[#F13361] px-1.5 text-[12px] text-white">
-                    {gameAsset?.assets?.[item.id]?.unclaim}
+          <div className="mb-[2.4vw] mt-[0.96vw] h-[1px] w-full bg-white/25 xl:mb-7.5 xl:mt-3"></div>
+          {assets.map((item) => (
+            <div className="mb-[1.92vw] xl:mb-6" key={item.id}>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <img src={item.icon} alt="capture-ball" className="h-[3.2vw] xl:size-10" />
+                  <div className="mx-2">
+                    <p className="text-[1.28vw]/[1.6vw] font-medium xl:text-base/5">{item.name}</p>
+                    <p className="w-[13vw] text-[0.96vw]/[1.6vw] text-gray-300 xl:w-[160px] xl:text-xs/5">{item.describe}</p>
                   </div>
-                </Button> : null}
+                  {gameAsset?.assets?.[item.id]?.unclaim ? (
+                    <Button
+                      type="yellow-shallow-2"
+                      className="relative h-[2.56vw] w-[8vw] overflow-visible text-[1.12vw]/[1.12vw] font-medium text-yellow xl:h-8 xl:w-25 xl:text-sm/3.5"
+                      onClick={() => onClaimGameAsset(item.id)}
+                    >
+                      Claim
+                      <div className="absolute right-0 top-0 flex h-4 min-w-4 translate-x-[50%] translate-y-[-50%] items-center justify-center rounded-full bg-[#F13361] px-1.5 text-[12px] text-white">
+                        {gameAsset?.assets?.[item.id]?.unclaim}
+                      </div>
+                    </Button>
+                  ) : null}
+                </div>
+                <div className="text-[1.6vw]/[1.6vw]  font-semibold text-yellow xl:text-xl/5">
+                  {gameAsset?.assets?.[item.id]?.unuse?.toLocaleString() ?? 0}
+                </div>
               </div>
-              <div className="text-[1.6vw]/[1.6vw]  font-semibold text-yellow xl:text-xl/5">{gameAsset?.assets?.[item.id]?.unuse?.toLocaleString() ?? 0}</div>
             </div>
-          </div>))
-         }
-          <Button type='yellow-dark' className='w-full mt-10 flex items-center justify-center' onClick={onOpenShop}>
-            <img src='/svg/shop.svg' className="w-[2vw] xl:w-[1.5vw] mr-1" />
+          ))}
+          <Button type="yellow-dark" className="mt-10 flex w-full items-center justify-center" onClick={onOpenShop}>
+            <img src="/svg/shop.svg" className="mr-1 w-[2vw] xl:w-[1.5vw]" />
             <span>Dragonverse Market</span>
           </Button>
+          {/* My Referral */}
+          <div className="mt-[3.84vw] flex items-center justify-between xl:mt-12">
+            <div className="text-[1.28vw]/[1.92vw] font-semibold xl:text-base/6">My Referral</div>
+            <div
+              className="flex cursor-pointer select-none items-center gap-1 text-[1.12vw]/[1.12vw] font-semibold text-blue xl:text-sm/3.5"
+              onClick={onOpenGameAssetLog}
+            >
+              History
+              <HistorySVG className="w-[1.28vw] xl:w-4" />
+            </div>
+          </div>
+          <div className="mb-[2.4vw] mt-[0.96vw] h-[1px] w-full bg-white/25 xl:mb-7.5 xl:mt-3">
+            <p className="text-[1.28vw]/[1.6vw] font-medium xl:text-base/5">Referral Link</p>
+          </div>
+
           <DrawerDepositWithdraw />
           <DrawerTradeLogs isOpen={isOpenTradeLogs} onOpenChange={setOpenTradeLogs} />
           <GameAssetsLog />
