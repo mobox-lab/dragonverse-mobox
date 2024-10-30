8570d88b4600189454d163b01f08663ca67860e9
joji-mb <545633740@qq.com>
2024-10-30 16:07:29 +0800

    feat: mobox reward ui

--- src/app/halloffame/_components/MyRewards.tsx
+++ src/app/halloffame/_components/MyRewards.tsx
@@ -80,6 +80,24 @@ export default function MyRewards() {
     };
   }, [merl, rewardsReceived]);
 
+  const moboxData = useMemo(() => {
+    // TODO: add mobox reward
+    // if (merl && rewardsReceived) {
+    //   const total = BigInt(merl!.amount || '0');
+    //   const received = BigInt(rewardsReceived[0]?.result?.[0] || '0');
+
+    //   return {
+    //     amount: formatNumber(total - received),
+    //     total: formatNumber(total),
+    //   };
+    // }
+
+    return {
+      amount: '0',
+      total: '0',
+    };
+  }, []);
+
   const onClaimMdbl = useCallback(async () => {
     if (mdblData.amount == '0' || claimMdblLoading) {
       return;
@@ -107,12 +125,25 @@ export default function MyRewards() {
     }).then();
   }, [address, writeContract, merl, merlClaimIsLoading]);
 
+  const onClaimMobox = useCallback(() => {
+    // TODO: add mobox reward
+    // if (moboxData.amount == '0' || moboxClaimIsLoading) {
+    //   return;
+    // }
+    // writeContract({
+    //   abi: LeaderboardRewardsABI,
+    //   functionName: 'usersReceiveMERLRewards',
+    //   args: [merl!.index + 1, merl!.amount, merl!.proof],
+    //   address: CONTRACT_ADDRESSES.claimMerl!,
+    // }).then();
+  }, []);
+
   return (
     <>
-      <div className="relative flex h-[15.04vw] flex-col items-center border border-gray-600 bg-black/60 pt-[1.6vw] backdrop-blur-sm xl:h-[188px] xl:pt-5 order-2">
+      <div className="relative order-2 flex h-[15.04vw] flex-col items-center border border-gray-600 bg-black/60 pt-[1.6vw] backdrop-blur-sm xl:h-[188px] xl:pt-5">
         <img
           src="/img/reward-bg-04.png"
-          className="absolute bottom-0 left-0 right-0 -z-10 w-full h-[75%] object-cover object-center"
+          className="absolute bottom-0 left-0 right-0 -z-10 h-[75%] w-full object-cover object-center"
         />
         <div className="text-[1.28vw]/[1.92vw] font-semibold xl:text-base/6">$MDBL</div>
         <div className="mt-[1.92vw] flex items-center justify-center xl:mt-6">
@@ -133,10 +164,10 @@ export default function MyRewards() {
           Claim
         </Button>
       </div>
-      <div className="relative flex h-[15.04vw] flex-col items-center border border-gray-600 bg-black/60 pt-[1.6vw] backdrop-blur-sm xl:h-[188px] xl:pt-5 order-4">
+      <div className="relative order-4 flex h-[15.04vw] flex-col items-center border border-gray-600 bg-black/60 pt-[1.6vw] backdrop-blur-sm xl:h-[188px] xl:pt-5">
         <img
           src="/img/reward-bg-05.png"
-          className="absolute bottom-0 left-0 right-0 -z-10 w-full h-[75%] object-cover object-center"
+          className="absolute bottom-0 left-0 right-0 -z-10 h-[75%] w-full object-cover object-center"
         />
         <div className="text-[1.28vw]/[1.92vw] font-semibold xl:text-base/6">$MERL</div>
         <div className="mt-[1.92vw] flex items-center justify-center xl:mt-6">
@@ -153,6 +184,28 @@ export default function MyRewards() {
           disabled={merlData.amount == '0'}
           loading={merlClaimIsLoading}
           onClick={onClaimMerl}
+        >
+          Claim
+        </Button>
+      </div>
+      <div className="relative order-5 flex h-[15.04vw] flex-col items-center border border-gray-600 bg-black/60 pt-[1.6vw] backdrop-blur-sm xl:h-[188px] xl:pt-5">
+        <img src="/img/reward-bg-06.png" className="absolute bottom-0 left-0 -z-10 h-full object-cover object-center" alt="" />
+        <div className="text-[1.28vw]/[1.92vw] font-semibold xl:text-base/6">MBOX</div>
+        <div className="mt-[1.92vw] flex items-center justify-center xl:mt-6">
+          <img src="/img/mobox.png" className="h-[2.24vw] xl:h-7" alt="mobox" />
+          <div className="ml-[0.64vw] text-[1.92vw]/[2.4vw] font-semibold text-yellow xl:ml-2 xl:text-2xl/7.5">
+            {moboxData.amount}
+          </div>
+        </div>
+        <p className="mt-0.5 text-xs/5 font-medium text-gray-300">Total: {moboxData.total}</p>
+        {/* TODO:  moboxClaimIsLoading */}
+        <Button
+          className="mt-[1.28vw] h-[2.56vw] w-[12.8vw] rounded-[0.16vw] py-0 text-[1.12vw]/[1.28vw] font-semibold text-yellow xl:mt-4 xl:h-8 xl:w-[160px] xl:rounded-sm xl:text-sm/4"
+          type="yellow-shallow"
+          loadingClassName="fill-yellow xl:w-3 xl:h-3 w-[0.96vw] h-[0.96vw]"
+          disabled={moboxData.amount == '0'}
+          // loading={moboxClaimIsLoading}
+          onClick={onClaimMobox}
         >
           Claim
         </Button>
