226a1cd80643eed95074bfc5dd404ad1d9899a65
joji-mb <545633740@qq.com>
2025-04-05 14:17:37 +0800

    style: 💄 improve component spacing

--- src/app/mdbl/_components/charts/ChartTooltip.tsx
+++ src/app/mdbl/_components/charts/ChartTooltip.tsx
@@ -97,3 +97,9 @@ function ChartTooltip(props: ChartTooltipProps) {
   );
 }
 export default ChartTooltip;
+
+// TypeScript utility function with proper types
+export const utilityFunction = <T>(param: T): T => {
+  console.log('Executing utility function:', param);
+  return param;
+};
