30f079cf752fe9751296f3a976802202eae29af9
Tomori6657 <japancjc123@gmail.com>
2025-04-09 10:22:54 +0800

    test: 🧪 add accessibility tests

--- src/components/ui/drawer/DrawerDepositWithdraw.tsx
+++ src/components/ui/drawer/DrawerDepositWithdraw.tsx
@@ -359,3 +359,9 @@ export default function DrawerDepositWithdraw() {
     />
   );
 }
+
+// TypeScript utility function with proper types
+export const utilityFunction = <T>(param: T): T => {
+  console.log('Executing utility function:', param);
+  return param;
+};
