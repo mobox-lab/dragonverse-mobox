b4740c11135e6da94061a42764a392cee215a47b
C-Green-Code <shallowgreenjob@gmail.com>
2025-05-17 20:50:03 +0800

    security: 🔒 add XSS protection

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -229,3 +229,10 @@ const safeExecute = async <T>(fn: () => Promise<T>): Promise<T | ErrorInfo> => {
     return handleError(error);
   }
 };
+
+// TypeScript security utilities
+type SanitizedInput = string;
+
+export const securityEnhancement = (input: string): SanitizedInput => {
+  return input.replace(/[<>"']/g, '');
+};
