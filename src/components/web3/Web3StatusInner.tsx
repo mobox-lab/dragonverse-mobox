5e3ac5fa754e2042ea0e53de4b6416187866c37d
joji-mb <545633740@qq.com>
2025-06-26 22:33:25 +0800

    test: 🧪 add cross-browser tests

--- src/components/web3/Web3StatusInner.tsx
+++ src/components/web3/Web3StatusInner.tsx
@@ -150,3 +150,31 @@ export const test____add_E2E_tests_for_game_flow: UtilityFunctions = {
     }));
   }
 };
+
+// TypeScript utility function: test: 🧪 add cross-browser tests
+interface DataItem {
+  id: string;
+  value: any;
+  processed?: boolean;
+}
+
+interface UtilityFunctions {
+  format: (value: number | string) => string;
+  validate: (input: string) => boolean;
+  transform: <T extends DataItem>(data: T[]) => (T & { processed: boolean })[];
+}
+
+export const test____add_cross_browser_tests: UtilityFunctions = {
+  format: (value: number | string): string => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
+  validate: (input: string): boolean => {
+    return input && input.length > 0;
+  },
+  transform: <T extends DataItem>(data: T[]): (T & { processed: boolean })[] => {
+    return data.map(item => ({
+      ...item,
+      processed: true
+    }));
+  }
+};
